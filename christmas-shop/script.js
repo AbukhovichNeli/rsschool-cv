09283623cd90edf9452b2a0796867ef7dc059a33
Obukhovich Nelli <obukhovichnelly@gmail.com>
2024-12-03 01:13:36 +0300

    feat: added button scroll to top

--- christmas-shop/script.js
+++ christmas-shop/script.js
@@ -115,9 +115,33 @@ fetch('./gifts.json')
             card.innerHTML =
                 `<img src="/img/${gift.category}.png" alt="${gift.name}">
                 <div class="card_text">
-                    <h4">${gift.category}</h4>
+                    <h4 style="color: ${getCategoryColor(gift.category)};">${gift.category}</h4>
                     <h3>${gift.name}</h3>
                 </div>`;
         });
     })
     .catch(error => console.error('Ошибка загрузки JSON:', error));
+    function getCategoryColor(category) {
+        if (category === "For Health") {
+            return '#FF43F7'; 
+        } else if (category === "For Work") {
+            return '#4361FF'; 
+        } else if (category === "For Harmony") {
+            return '#06A44F'; 
+        };
+    }
+
+    const sliderToTop = document.querySelector(".scrollUP");
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            sliderToTop.style.display = "block"; 
+        } else {
+            sliderToTop.style.display = "none";
+        }
+    });
+    sliderToTop.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
