51f875443a095e9c67adbe7bd5574aef2ab25236
Obukhovich Nelli <obukhovichnelly@gmail.com>
2024-12-02 14:26:36 +0300

    feat: added action to burger(close/open)

---
+++ christmas-shop/script.js
@@ -0,0 +1,23 @@
+//меню
+const burger_btn = document.querySelector(".burger");
+const menu = document.querySelector(".burger_menu_section");
+let isMenuOpen = false;
+function toggleMenu() {
+    if (isMenuOpen) {
+        menu.style.display = "";
+        burger_btn.style.backgroundImage = 'url(./img/Burger.png)';
+    }
+    else {
+        menu.style.display = "flex";
+        burger_btn.style.backgroundImage = 'url(./img/Burger_close.png)';
+    }
+    isMenuOpen = !isMenuOpen;
+}
+burger_btn.addEventListener('click', toggleMenu);
+let links = document.querySelectorAll(".menu_item_small");
+links.forEach(link => {
+    link.addEventListener('click', function () {
+        if (isMenuOpen) toggleMenu();
+    })
+})
+console.log(linkMenu);
